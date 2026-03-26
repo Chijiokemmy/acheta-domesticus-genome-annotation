fcd28c6dde10cb3fa567ca4603b2336e2b0f91f4
Emmanuel Odii <mrchijiokemmy@gmail.com>
2026-03-26 19:12:05 -0400

    Add braker_run.sh script for genome annotation

---
+++ scripts/braker_run.sh
@@ -0,0 +1,9 @@
+braker.pl \
+--genome="$GENOME" \
+--bam="$BAM" \
+--prot_seq="$PROT" \
+--species=acheta_domesticus_braker3 \
+--workingdir=/work \
+--AUGUSTUS_CONFIG_PATH=/work/augustus_config \
+--threads=16 \
+--etpmode
